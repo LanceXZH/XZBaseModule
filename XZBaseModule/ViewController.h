afdc5faf0550a57c0b025134372340ded7532176
xuzhong <xuzhong@meituan.com>
2018-03-01 14:31:47 +0800

    Initial Commit

---
+++ XZBaseModule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZBaseModule
+//
+//  Created by xuzhong on 2018/3/1.
+//  Copyright © 2018年 轻量未来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
